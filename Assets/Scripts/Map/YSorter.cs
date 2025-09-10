d7e4cc9224d28657a45d5e38957243223135c2f2
Võ Trần Minh Trí <trivtmce182301@fpt.edu.vn>
2025-09-10 09:06:08 +0700

    add collider and sorting Y layer when player moving

---
+++ Assets/Scripts/Map/YSorter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering; // cần cho SortingGroup
+
+public class YSorter : MonoBehaviour
+{
+    public int offset = 0;       // bù trừ nếu muốn player luôn trội hơn 1 chút
+    public int multiplier = 100; // độ phân giải sắp xếp
+
+    SortingGroup sg;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        sg = GetComponent<SortingGroup>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        int order = -(int)(transform.position.y * multiplier) + offset;
+
+        if (sg != null)          // nếu có Sorting Group (nhân vật nhiều sprite)
+            sg.sortingOrder = order;
+        else if (sr != null)     // nhân vật 1 sprite
+            sr.sortingOrder = order;
+    }
+}
